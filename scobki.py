a0b4f5c128ef516f3d5565a6c569013544da95ad
vasadaz <vasadaz@ya.ru>
2021-04-09 16:45:40 +0300

    Ebanye scobky

--- scobki.py
+++ scobki.py
@@ -1,24 +1,46 @@
 # Функция может обращаться к себеже
-def get_depth(l):
-    if isinstance(l, list):
-        t = ()
-        for itm in l:
-            t += get_depth(itm)
-        return 1 + max(t)
-    return 0
+def skoba(old_list: list, in_list: list, corect_list: list):
+    copy_old_list = old_list.copy()
+    corect_in_list = 0
+    for el in old_list:
+        corect_in_list += 1
+        if corect_list[-1] != 0:
+            corect_list[-1] -= 1
+            continue
+        elif len(corect_list) != 1:
+            del corect_list[-1]
+
+        if el == "(":
+            copy_old_list.pop(0)
+            in_list.append([])
+            skoba(copy_old_list, in_list[-1], corect_list)
+        elif el == ")":
+            copy_old_list.pop(0)
+            corect_list.append(corect_in_list)
+            return
+        else:
+            copy_old_list.pop(0)
+            in_list.append(el)
+
+    return
 
 # Признак fuck = 1 для остановки цикла
-fuck = 0
+__FUCK = 0
+__CORECTOR_LIST_SKOBKI = [0]
 
-while fuck == 0:
+while __FUCK == 0:
     # Ввод примера
     primer = input("Чё там?\n")
     if primer == "n":
-        fuck = 1
+        __FUCK = 1
         break
+
+    filter_for_primer = lambda n: [] if n == "(" else n
     # Создание списка из строки primer с фильтром от пробелов
     list_primer = [el for el in primer if el != " "]
 
-m = ['3', '4', '2', '2', '1', '4', '1', ['+', '+', '(', '+', '(', '-', ')', [['*'], ')', '-']]]
+    print(list_primer)
+    new_m = []
 
-print(get_depth(m))
+    skoba(list_primer, new_m, __CORECTOR_LIST_SKOBKI)
+    print("\n\n\n",new_m)
