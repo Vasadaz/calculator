a0fdd7e7ebf8f1e2046515b5707dae7b8eb91956
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2021-04-10 13:18:18 +0300

    Negative numbers

--- calculator.py
+++ calculator.py
@@ -32,79 +32,98 @@ while __FUCK == 0:
         break
     # Создание списка из строки primer с фильтром от пробелов
     list_primer = [el for el in primer if el != " "]
-
-    # Пример после скобок
-    new_primer = []
-    scobki.fun_brackets_in_list(list_primer, new_primer)
+    print(list_primer)
 
     # Список для формирования чисел из примера
-    list_digit = []
+    list_args = []
 
-    # Список для формирования опереторов из примера
-    list_operator = []
+    # Список для формирования чисел из примера cо скобками
+    list_args_brackets = []
 
-    # Коректор индекса для звписи в элемент списка list_digit
-    i_for_filter = 0
+    # Список для формирования опереторов из примера cо скобками
+    list_operators_brackets = []
+
+    # Коректор индекса для звписи в элемент списка list_args_brackets
+    __i_for_filter = 0
 
 
-    # Скелеивание единых чисел в элемент списока list_digit и
-    # добавление арифмитического оператора в список list_operator
+    # Скелеивание единых чисел в элемент списока list_args_brackets и
+    # добавление арифмитического оператора в список list_operators_brackets
     for i in range(len(list_primer)):
         # Замена запятой на точку
         list_primer[i] = "." if list_primer[i] == "," else list_primer[i]
+        print(list_primer[i:i + 2])
+        # Обработка отрицательных чисел
+        if (i == 0 and list_primer[i] == "-") or list_primer[i - 1 :i + 1] == ['(', '-']:
+            list_args_brackets.append(list_primer[i])
 
         # Фильтр для определения целых и точечных чисел
-        if list_primer[i].isdigit() or list_primer[i] == ".":
+        elif list_primer[i].isdigit() or list_primer[i] == ".":
             # Перенаправление ошибки, т.к. элемент с данным индексом может не существовать
             try:
-                # Добавление к существуещему элементу в списке list_digit
-                list_digit[i_for_filter] += list_primer[i]
+                # Добавление к существуещему элементу в списке list_args_brackets
+                list_args_brackets[__i_for_filter] += list_primer[i]
             except IndexError:
-                # Создание элемента в списке list_digit
-                list_digit.append(list_primer[i])
+                # Создание элемента в списке list_args_brackets
+                list_args_brackets.append(list_primer[i])
 
-        # Запись в сисок list_operator арифмитического оператора **
+        # Помечаем скобки в оба новых списка
+        elif list_primer[i] == "(" or list_primer[i] == ")":
+            list_args_brackets.append(list_primer[i])
+            list_operators_brackets.append(list_primer[i])
+            __i_for_filter += 1
+        # Запись в сисок list_for_operators арифмитического оператора **
         elif list_primer[i + 1] == "*":
-            list_operator.append("**")
-            # Смещение индекса для следующего числа в списке list_digit
-            i_for_filter += 1
+            list_operators_brackets.append("**")
+            # Смещение индекса для следующего числа в списке list_args_brackets
+            __i_for_filter += 1
             # Защита от дублирования * в следующей итерации
             list_primer[i + 1] = "DEL"
 
-            # Запись в сисок list_operator арифмитического оператора //
+            # Запись в сисок list_for_operators арифмитического оператора //
         elif list_primer[i + 1] == "/":
-            list_operator.append("//")
-            # Смещение индекса для следующего числа в списке list_digit
-            i_for_filter += 1
+            list_operators_brackets.append("//")
+            # Смещение индекса для следующего числа в списке list_args_brackets
+            __i_for_filter += 1
             # Защита от дублирования / в следующей итерации
             list_primer[i + 1] = "DEL"
 
-        # Запись в сисок list_operator арифмитических операторов + - * / ^
+        # Запись в сисок list_for_operators арифмитических операторов + - * / ^
         elif list_primer[i] != "DEL":
-            list_operator.append(list_primer[i])
-            # Смещение индекса для следующего числа в списке list_digit
-            i_for_filter += 1
-        # print(i_for_filter, list_digit, list_operator)
+            list_operators_brackets.append(list_primer[i])
+            # Смещение индекса для следующего числа в списке list_args_brackets
+            __i_for_filter += 1
+        print(__i_for_filter, list_args_brackets, list_operators_brackets)
+
+    # Список для формирования чисел из примера без скобок () --> []
+    list_args = []
+    scobki.fun_brackets_in_list(list_args_brackets, list_args)
+
+    # Список для формирования опереторов из примера без скобок () --> []
+    list_operators = []
+    scobki.fun_brackets_in_list(list_operators_brackets, list_operators)
+
+    print(list_args, list_operators)
 
     # Выполнение возведение в степень: ** ^
-    for i in range(len(list_operator)):
-        # print(list_digit, list_operator)
+    for i in range(len(list_operators)):
+        # print(list_args_brackets, list_for_operators)
         # Обработка операторов ** ^
-        if list_operator[i] == "**" or list_operator[i] == "^":
+        if list_operators[i] == "**" or list_operators[i] == "^":
             # Присвоение класса для обработки операции
             n = ArifmeticOperation()
-            # Перезапись резальтата обработки в список list_digit[с этим индексом будет валняться следующая операция]
-            list_digit[i + 1] = n.operation(float(list_digit[i]), list_operator[i], float(list_digit[i + 1]))
-            # Удаление уже прооперируемого числа из списка list_digit без смещения по индексу
-            list_digit[i] = "DEL"
-            # Удаление уже прооперируемого перанта из списка list_operator без смещения по индексу
-            list_operator[i] = "DEL"
+            # Перезапись резальтата обработки в список list_args_brackets[с этим индексом будет валняться следующая операция]
+            list_args[i + 1] = n.operation(float(list_args[i]), list_operators[i], float(list_args[i + 1]))
+            # Удаление уже прооперируемого числа из списка list_args_brackets без смещения по индексу
+            list_args[i] = "DEL"
+            # Удаление уже прооперируемого перанта из списка list_for_operators без смещения по индексу
+            list_operators[i] = "DEL"
 
-    # Формированиее списка чисел возведённых в степень из списка list_digit отфильтрованный от "DEL"
-    pow_list_digit = [el for el in list_digit if el != "DEL"]
+    # Формированиее списка чисел возведённых в степень из списка list_args_brackets отфильтрованный от "DEL"
+    pow_list_digit = [el for el in list_args if el != "DEL"]
 
-    # Формированиее списка арифмитических операций из списка list_operator отфильтрованный от "DEL"
-    pow_list_operator = [el for el in list_operator if el != "DEL"]
+    # Формированиее списка арифмитических операций из списка list_for_operators отфильтрованный от "DEL"
+    pow_list_operator = [el for el in list_operators if el != "DEL"]
 
     # Выполнение приоритетных арифмитических операций: * / // %
     for i in range(len(pow_list_operator)):
@@ -118,9 +137,9 @@ while __FUCK == 0:
             # pow_list_operator[с этим индексом будет валняться следующая операция]
             pow_list_digit[i + 1] = n.operation(float(pow_list_digit[i]), pow_list_operator[i],
                                                 float(pow_list_digit[i + 1]))
-            # Удаление уже прооперируемого числа из списка list_digit без смещения по индексу
+            # Удаление уже прооперируемого числа из списка list_args_brackets без смещения по индексу
             pow_list_digit[i] = "DEL"
-            # Удаление уже прооперируемого перанта из списка list_operator без смещения по индексу
+            # Удаление уже прооперируемого перанта из списка list_for_operators без смещения по индексу
             pow_list_operator[i] = "DEL"
 
     # Формированиее списка чисел из списка pow_list_digit отфильтрованный от "DEL"
--- pizdel.py
+++ pizdel.py
@@ -1,77 +1,69 @@
 import scobki
 
-# Пример после скобок
-list_primer = ['3', '+', '4', '+', ['2', '+', ['2', '-', '1'], '*', '4'], '-', '1']
-new_primer = []
-scobki.fun_brackets_in_list(list_primer, new_primer)
+list_primer = ['-', '3', '+', '4', '+', '(', '2', '+', '(', '-', '2', '-', '1', ')', '*', '4', ')', '-', '1']
 
 # Список для формирования чисел из примера
-list_args = []
+list_args_brackets = []
 
 # Список для формирования опереторов из примера
-list_operators = []
+list_operators_brackets = []
 
-# Коректор индекса для звписи в элемент списка list_args
+# Коректор индекса для звписи в элемент списка list_args_brackets
 __i_for_filter = 0
-
-
-def razdelitel(list_math_ex: list, list_for_args: list, list_for_operators: list):
-    copy_list_math_ex = list_math_ex.copy()
-    for i in range(len(list_math_ex)):
-        if isinstance(i, list):
-            list_for_args.append([])
-            list_for_operators.append([])
-            razdelitel(copy_list_math_ex[i], list_for_args[-1], list_for_operators[-1])
-        else:
-            sortirovka(list_math_ex, i, list_for_args, list_for_operators)
-
-    return None
 
 
 # Скелеивание единых чисел в элемент списока list_args_brackets и
 # добавление арифмитического оператора в список list_for_operators
-
-def sortirovka(list_math_ex: list, i_list_math_ex: int, list_for_args: list, list_for_operators: list):
-    global __i_for_filter
+for i in range(len(list_primer)):
     # Замена запятой на точку
-    list_math_ex[i_list_math_ex] = "." if list_math_ex[i_list_math_ex] == "," else list_math_ex[i_list_math_ex]
+    list_primer[i] = "." if list_primer[i] == "," else list_primer[i]
+    print(list_primer[i:i + 2])
+    # Обработка отрицательных чисел
+    if (i == 0 and list_primer[i] == "-") or list_primer[i - 1 :i + 1] == ['(', '-']:
+        list_args_brackets.append(list_primer[i])
 
     # Фильтр для определения целых и точечных чисел
-    if list_math_ex[i_list_math_ex].isdigit() or list_math_ex[i_list_math_ex] == ".":
+    elif list_primer[i].isdigit() or list_primer[i] == ".":
         # Перенаправление ошибки, т.к. элемент с данным индексом может не существовать
         try:
             # Добавление к существуещему элементу в списке list_args_brackets
-            list_for_args[__i_for_filter] += list_math_ex[i_list_math_ex]
+            list_args_brackets[__i_for_filter] += list_primer[i]
         except IndexError:
             # Создание элемента в списке list_args_brackets
-            list_for_args.append(list_math_ex[i_list_math_ex])
+            list_args_brackets.append(list_primer[i])
 
+    # Помечаем скобки в оба новых списка
+    elif list_primer[i] == "(" or list_primer[i] == ")":
+        list_args_brackets.append(list_primer[i])
+        list_operators_brackets.append(list_primer[i])
+        __i_for_filter += 1
     # Запись в сисок list_for_operators арифмитического оператора **
-    elif list_math_ex[i_list_math_ex + 1] == "*":
-        list_for_operators.append("**")
+    elif list_primer[i + 1] == "*":
+        list_operators_brackets.append("**")
         # Смещение индекса для следующего числа в списке list_args_brackets
         __i_for_filter += 1
         # Защита от дублирования * в следующей итерации
-        list_math_ex[i_list_math_ex + 1] = "DEL"
+        list_primer[i + 1] = "DEL"
 
         # Запись в сисок list_for_operators арифмитического оператора //
-    elif list_math_ex[i_list_math_ex + 1] == "/":
-        list_for_operators.append("//")
+    elif list_primer[i + 1] == "/":
+        list_operators_brackets.append("//")
         # Смещение индекса для следующего числа в списке list_args_brackets
         __i_for_filter += 1
         # Защита от дублирования / в следующей итерации
-        list_math_ex[i_list_math_ex + 1] = "DEL"
+        list_primer[i + 1] = "DEL"
 
     # Запись в сисок list_for_operators арифмитических операторов + - * / ^
-    elif list_math_ex[i_list_math_ex] != "DEL":
-        list_for_operators.append(list_math_ex[i_list_math_ex])
+    elif list_primer[i] != "DEL":
+        list_operators_brackets.append(list_primer[i])
         # Смещение индекса для следующего числа в списке list_args_brackets
         __i_for_filter += 1
-    # print(__i_for_filter, list_args_brackets, list_for_operators)
-    __i_for_filter = 0
-    return None
+    print(__i_for_filter, list_args_brackets, list_operators_brackets)
 
-razdelitel(new_primer, list_args, list_operators)
+list_args = []
+scobki.fun_brackets_in_list(list_args_brackets, list_args)
 
-print(list_args)
-print(list_operators)
+list_operators = []
+scobki.fun_brackets_in_list(list_operators_brackets, list_operators)
+
+print(list_args, list_operators)
