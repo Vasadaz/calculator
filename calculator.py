1b9ccf5a5dd649e7f295da6d8cd12f56c6f18009
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2021-04-26 16:03:09 +0300

    Update calculator.py
    Refresh upper comment

--- calculator.py
+++ calculator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-The original comments are written in Russian.
-
 My first experience of developing a Python program.
 My goal was to create a complete calculator that can:
       1) Accept a string converting it into mathematical operations
       2) Handle numbers with comma or point
-      4) Perform mathematical operations + - / //% ** ^
-      5) Processing mathematical examples with brackets.
-      6) No third-party libraries were used
+      3) Perform mathematical operations + - / //% ** ^
+      4) Processing mathematical examples with brackets.
+      5) No third-party libraries were used
 
+The original comments are written in Russian.
 Мой первый опыт разработки программы на Python.
 Перед мной стояла цель создать полноценный калькулятор, который может:
     1) Принимать строку преобразуя её в математические операции
     2) Обрабатывать числа с запятой или точкой
-    4) Выполнять математические операции + - / // % ** ^
-    5) Обработка математических примеров со скобками
-    6) Не использовались сторонние библиотек
+    3) Выполнять математические операции + - / // % ** ^
+    4) Обработка математических примеров со скобками
+    5) Не использовались сторонние библиотек
 """
 
 
